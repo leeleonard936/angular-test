f75e802b58412dc5dbe46cd533d00b22059b1a33
Leonardo Conroy <leeleonard936@gmail.com>
2024-05-10 10:49:34 -0400

    modal and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatCardModule } from '@angular/material/card'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {Dialog, DialogRef, DIALOG_DATA, DialogModule} from '@angular/cdk/dialog';
 
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatCardModule, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'test-app';
+  constructor(public dialog: Dialog) {}
+  openDialog(): void {
+    const dialogRef = this.dialog.open<string>(TestDialogModal, {
+      width: '500px',
+      data: {fname: this.fnValue, lname: this.lnValue, email: this.eValue},
+    });
+  }
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  fnValue='';
+  lnValue='';
+  eValue='';
+}
+@Component({
+  selector: 'DialogModal',
+  templateUrl: './app.dialogModal.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [FormsModule, MatButtonModule],
+})
+export class TestDialogModal {
+  constructor(
+    public dialogRef: DialogRef<string>,
+    @Inject(DIALOG_DATA) public data: FormData,
+  ) {}
 }
